ba2cedc7997d13439cf0a7bf6b54321472b19921
ashtam55 <ashtam.delhi@gmail.com>
2019-04-01 18:15:48 +0530

    Code updation

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,27 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#define USE_SERIAL Serial
-#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 
-#define NUM_LEDS 60
-#define BRIGHTNESS 50
-#define PIN 5 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+
+#define USE_SERIAL Serial
+
 
 // Update these with values suitable for your network.
 
-const char* ssid = "91a";
+const char* ssid = "ESP";
 const char* password = "123456789q";
 const char* mqtt_server = "akriya.co.in";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
- 
+
 int delayval = 5000;
 
-
+int num = 0;
+int pin = 2;
 
 void setup_wifi() {
 
@@ -42,9 +44,6 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 
 
-  strip.setBrightness(BRIGHTNESS);
-  strip.begin();
-  strip.show();
 }
 
 
@@ -56,58 +55,37 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-  Serial.println();
+
+  String TOP = topic;
+
+if(TOP == "91s/springboardKarta-otaEnabled"){
 
   pinMode(2,HIGH);
   delay(500);
   pinMode(2,LOW);
 
-  //   String q = "";
-  // q = (char)payload[0];
-  // q = q + (char)payload[1];
-  // int num = q.toInt();
+ String q = "";
+  q = (char)payload[0];
+  q = q + (char)payload[1];
+   num = q.toInt();
 
-  //  Serial.println(num);
+   Serial.println(num);
 
+}
 
-
-
-for(int j=0;j<NUM_LEDS;j++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.setPixelColor(j, strip.Color(255,255,0)); // Moderately bright green color.
-
-    strip.show(); // This sends the updated pixel color to the hardware.
-
-    // delay(delayval); // Delay for a period of time (in milliseconds).
-   
-  }
-
-delay(delayval);
-
-  for(int k=0;k<=NUM_LEDS;k++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.setPixelColor(k, strip.Color(0,0,0)); // Moderately bright green color.
-
-    strip.show(); // This sends the updated pixel color to the hardware.
-
-    // delay(delayval); // Delay for a period of time (in milliseconds).
-   
-  }
 
 }
 
 void setup() {
-  
+
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  pinMode(2, OUTPUT);
-  pinMode(2,LOW);
-    String mac = WiFi.macAddress();
-    Serial.println(mac);
+  pinMode(pin, OUTPUT);
+  pinMode(pin, LOW);
+  String mac = WiFi.macAddress();
+  Serial.println(mac);
 }
 
 void reconnect() {
@@ -120,6 +98,7 @@ void reconnect() {
       // Once connected, publish an announcement...
 
       client.subscribe("91s/intercom");
+      client.subscribe("91s/springboardKarta-otaEnabled");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -136,6 +115,25 @@ void loop() {
   }
   client.loop();
 
-  
-}
+    pinMode(pin,LOW);
+    delay(1000);
+    pinMode(pin,HIGH);
 
+    if (num == 1 && WiFi.status() == WL_CONNECTED){
+  Serial.println("Connected, updating");
+  t_httpUpdate_return ret = ESPhttpUpdate.update("https://ashtam55.github.io/.bin", "", "70 0B 6F 62 4F 41 EB 1A 42 3F 73 5A DA 96 98 2D 7F 2B 75 6F");
+        switch(ret) {
+            case HTTP_UPDATE_FAILED:
+                USE_SERIAL.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+                break;
+
+            case HTTP_UPDATE_NO_UPDATES:
+                USE_SERIAL.println("HTTP_UPDATE_NO_UPDATES");
+                break;
+
+            case HTTP_UPDATE_OK:
+                USE_SERIAL.println("HTTP_UPDATE_OK");
+                break;
+        }
+}
+}
